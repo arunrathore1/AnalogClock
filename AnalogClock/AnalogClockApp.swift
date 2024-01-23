08e3c68dad3ff5691f88ae97b8126235035d7e32
Arun Rathore <arathore@nowoptics.com>
2024-01-23 22:59:29 +0530

    initial commits

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by Arun Singh Rathore on 12/01/24.
+//  
+    
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
